56a656baa6fc319d856b63e6167dc34c467421a9
rongjie.duan@autocore.ai <rongjie.duan@autocore.ai>
2021-02-03 15:32:24 +0800

    update

--- src/main copy.rs
+++ src/main copy.rs
@@ -1,7 +1,7 @@
 // 1. 启动一个服务
 // 2. 等待对方发送POST请求
 // 3. 当请求参数a的值为1的时候，使用zenoh put 信息
-
+use async_std::stream::StreamExt;
 use serde::{Deserialize, Serialize};
 use tide::prelude::*; // Pulls in the json! macro.
 use tide::{Body, Request};
@@ -11,15 +11,18 @@ use zenoh::*;
 #[derive(Deserialize, Serialize)]
 struct Path {
     path: String,
-    value: Integer,
+    value: u32,
 }
 
-async fn put_message(path, value) {
+async fn put_message(path: String, value:u32) {
     // initiate logging
-    env_logger::init();
-
-    let path = mess;
+    // env_logger::init();
+    println!("Enter put_message");
+    let path = path;
     let value = value;
+
+    let config = Properties::default();
+
     println!("New zenoh...");
     let zenoh = Zenoh::new(config.into()).await.unwrap();
 
@@ -28,48 +31,38 @@ async fn put_message(path, value) {
 
     println!("Put Data ('{}': '{}')...\n", path, value);
     workspace
-        .put(&path.try_into().unwrap(), value.into())
+        .put(&path.try_into().unwrap(), value.to_string().into())
         .await
         .unwrap();
-
-    // --- Examples of put with other types:
-
-    // - Integer
-    // workspace.put(&"/demo/example/Integer".try_into().unwrap(), 3.into())
-    //     .await.unwrap();
-
-    // - Float
-    // workspace.put(&"/demo/example/Float".try_into().unwrap(), 3.14.into())
-    //     .await.unwrap();
-
-    // - Properties (as a Dictionary with str only)
-    // workspace.put(
-    //         &"/demo/example/Properties".try_into().unwrap(),
-    //         Properties::from("p1=v1;p2=v2").into()
-    //     ).await.unwrap();
-
-    // - Json (str format)
-    // workspace.put(
-    //         &"/demo/example/Json".try_into().unwrap(),
-    //         Value::Json(r#"{"kind"="memory"}"#.to_string()),
-    //     ).await.unwrap();
-
-    // - Raw ('application/octet-stream' encoding by default)
-    // workspace.put(
-    //         &"/demo/example/Raw".try_into().unwrap(),
-    //         vec![0x48u8, 0x69, 0x33].into(),
-    //     ).await.unwrap();
-
-    // - Custom
-    // workspace.put(
-    //         &"/demo/example/Custom".try_into().unwrap(),
-    //         Value::Custom {
-    //             encoding_descr: "my_encoding".to_string(),
-    //             data: vec![0x48u8, 0x69, 0x33].into(),
-    //     }).await.unwrap();
-
-    zenoh.close().await.unwrap();
+    // zenoh.close().await.unwrap();
+    ()
 }
+
+
+async fn get_message() {
+    let config = Properties::default();
+
+    println!("New zenoh...");
+    let zenoh = Zenoh::new(config.into()).await.unwrap();
+
+    println!("New workspace...");
+    let workspace = zenoh.workspace(None).await.unwrap();
+    let selector = "/demo/example/test";
+    println!("Get Data from {}'...\n", selector);
+    let mut data_stream = workspace.get(&selector.try_into().unwrap()).await.unwrap();
+    while let Some(data) = data_stream.next().await {
+        println!(
+            "  {} : {:?} (encoding: {} , timestamp: {})",
+            data.path,
+            data.value,
+            data.value.encoding_descr(),
+            data.timestamp
+        )
+    }
+
+    // zenoh.close().await.unwrap();
+}
+
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
@@ -79,14 +72,23 @@ async fn main() -> tide::Result<()> {
     app.at("/submit").post(|mut req: Request<()>| async move {
         let path: Path = req.body_json().await?;
         println!("Path: {}, Value: {}", path.path, path.value);
-
-        if path.value != 1 {
-            put_message(path.path, path.value).await?;
+        let p = path.path;
+        let v = path.value;
+        if p == "/demo/example/test" {
+            put_message(p, v).await;
         } else {
-            println!("value is not equal 1, can't publish path/value, path: {}, Value: {}", path.path, path.value);
+            println!("value is not equal 1, can't publish path/value, path: {}, Value: {}", &p, &v);
         }
 
-        Ok(Body::from_json(&cat)?)
+
+        Ok(Body::from_json(&9)?)
+    });
+
+    app.at("/get").get(|_| async {
+        get_message().await;
+        Ok(json!({
+            "get": "successfully",
+        }))
     });
 
     app.listen("127.0.0.1:8080").await?;
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,15 @@
-// 1. 启动一个服务
-// 2. 等待对方发送POST请求
-// 3. 当请求参数a的值为1的时候，使用zenoh put 信息
-
-use serde::{Deserialize, Serialize};
-use tide::prelude::*; // Pulls in the json! macro.
-use tide::{Body, Request};
+use futures::prelude::*;
 use std::convert::TryInto;
 use zenoh::*;
 
-#[derive(Deserialize, Serialize)]
-struct Path {
-    path: String,
-    value: u32,
-}
+use clap::{App, Arg};
 
-async fn put_message(path: String, value:u32) {
+#[async_std::main]
+async fn main() {
     // initiate logging
-    // env_logger::init();
-    println!("Enter put_message");
-    let path = path;
-    let value = value;
+    env_logger::init();
 
-    let mut config = Properties::default();
-    // for key in ["mode", "peer", "listener"].iter() {
-    //     config.insert(key.to_string(), key.to_string());
-    // }
+    let (config, selector) = parse_args();
 
     println!("New zenoh...");
     let zenoh = Zenoh::new(config.into()).await.unwrap();
@@ -32,35 +17,62 @@ async fn put_message(path: String, value:u32) {
     println!("New workspace...");
     let workspace = zenoh.workspace(None).await.unwrap();
 
+    let path = "/demo/example/test";
+    let value = "123";
     println!("Put Data ('{}': '{}')...\n", path, value);
     workspace
-        .put(&path.try_into().unwrap(), value.to_string().into())
+        .put(&path.try_into().unwrap(), value.into())
         .await
         .unwrap();
+
+    println!("Get Data from {}'...\n", selector);
+    let mut data_stream = workspace.get(&selector.try_into().unwrap()).await.unwrap();
+    while let Some(data) = data_stream.next().await {
+        println!(
+            "  {} : {:?} (encoding: {} , timestamp: {})",
+            data.path,
+            data.value,
+            data.value.encoding_descr(),
+            data.timestamp
+        )
+    }
+
     zenoh.close().await.unwrap();
-    ()
 }
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
-    tide::log::start();
-    let mut app = tide::new();
+fn parse_args() -> (Properties, String) {
+    let args = App::new("zenoh get example")
+        .arg(
+            Arg::from_usage("-m, --mode=[MODE] 'The zenoh session mode.")
+                .possible_values(&["peer", "client"])
+                .default_value("peer"),
+        )
+        .arg(Arg::from_usage(
+            "-e, --peer=[LOCATOR]...  'Peer locators used to initiate the zenoh session.'",
+        ))
+        .arg(Arg::from_usage(
+            "-l, --listener=[LOCATOR]...   'Locators to listen on.'",
+        ))
+        .arg(
+            Arg::from_usage("-s, --selector=[SELECTOR] 'The selection of resources to get'")
+                .default_value("/demo/example/test"),
+        )
+        .arg(Arg::from_usage(
+            "--no-multicast-scouting 'Disable the multicast-based scouting mechanism.'",
+        ))
+        .get_matches();
 
-    app.at("/submit").post(|mut req: Request<()>| async move {
-        let path: Path = req.body_json().await?;
-        println!("Path: {}, Value: {}", path.path, path.value);
-        let p = path.path;
-        let v = path.value;
-        if p == "/demo/example/test" {
-            put_message(p, v).await;
-        } else {
-            println!("value is not equal 1, can't publish path/value, path: {}, Value: {}", &p, &v);
+    let mut config = Properties::default();
+    for key in ["mode", "peer", "listener"].iter() {
+        if let Some(value) = args.values_of(key) {
+            config.insert(key.to_string(), value.collect::<Vec<&str>>().join(","));
         }
+    }
+    if args.is_present("no-multicast-scouting") {
+        config.insert("multicast_scouting".to_string(), "false".to_string());
+    }
 
+    let selector = args.value_of("selector").unwrap().to_string();
 
-        Ok(Body::from_json(&9)?)
-    });
-
-    app.listen("127.0.0.1:8080").await?;
-    Ok(())
+    (config, selector)
 }
